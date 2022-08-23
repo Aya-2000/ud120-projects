9f29b02111a9209ef900756174a63bed0bbc73f7
Aya-Ahmed-Gamal <19P1689@eng.asu.edu.eg>
2022-08-23 19:43:15 +0200

    Update validate_poi.py

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,21 +12,28 @@
 import os
 import joblib
 import sys
-sys.path.append(os.path.abspath("../tools/"))
-from feature_format import featureFormat, targetFeatureSplit
+sys.path.append("../UD120-PROJECTS")
+from tools.feature_format import featureFormat, targetFeatureSplit
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
-data_dict = joblib.load(open("../final_project/final_project_dataset.pkl", "rb") )
+data_dict = joblib.load(open("../UD120-PROJECTS/final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
+sort_keys = '../tools/python2_lesson13_keys.pkl'
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
 
 ### it's all yours from here forward!  
-
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_list,labels)
+pred=clf.predict(labels)
+acc = accuracy_score(pred,labels)
+print(acc)
 
